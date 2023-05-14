35e5028fdf93414000711f76319655a4d4b0b5e2
Duvan Cruz <duvandres9820@gmail.com>
2023-05-13 21:34:07 -0500

    filtros corregidos

--- PI-Food-main/api/src/controllers/recipesControllers/getQueryRecipe.js
+++ PI-Food-main/api/src/controllers/recipesControllers/getQueryRecipe.js
@@ -7,21 +7,25 @@ si no existe este nombre arrojar error.*/
 const { getAllRecipes }= require("../getInfo")
 
 
-const getQueryRecipe= async (req, res)=>{
-    const {name}= req.query;
-    const allRecipes= await getAllRecipes();
+const getQueryRecipe = async (req, res) => {
+    const { name } = req.query;
+    const allRecipes = await getAllRecipes();
     try {
-        if(!name) res.status(200).json(allRecipes);
-        const recipesFilter= await allRecipes.filter(recipe=> recipe.title.toLowerCase()=== name.toLowerCase());
-        if(recipesFilter.length===0) throw Error("No se encontro RECETA");
-        res.status(200).json(recipesFilter)
-        
+      if (!name) {
+        res.status(200).json(allRecipes);
+      } else {
+        const recipesFilter = allRecipes.filter(
+          (recipe) => recipe.title.toLowerCase() === name.toLowerCase()
+        );
+        if (recipesFilter.length === 0) {
+          throw new Error("No se encontro RECETA");
+        }
+        res.status(200).json(recipesFilter);
+      }
     } catch (error) {
-        res.status(404).json({error: error.message})
+      res.status(404).json({ error: error.message });
     }
-   
-}
-    
+  };
 
 
 module.exports=getQueryRecipe
