03ff3f0493be78ea8e8883999ea6f9330fe47d14
duvandres9820@gmail.com <duvandres9820@gmail.com>
2023-09-03 21:05:45 +0200

    deploy

--- PI-Food-main/api/src/controllers/getInfo.js
+++ PI-Food-main/api/src/controllers/getInfo.js
@@ -1,10 +1,13 @@
 //Este archivo se encarga de pedir la info necesaria para que la puedan utilizar los controladores
 
 
-require('dotenv').config(); // Cargamos las variables de entorno desde el archivo .env 
+
+require('dotenv').config(); // Cargamos las variables de entorno desde el archivo .env
+//const axios= require("axios") //importamos axios
+//const { API_KEY } = process.env; // Obtenemos las variables de entorno API_KEY y DB_HOST
+//require('dotenv').config(); // Cargamos las variables de entorno desde el archivo .env 
 const { Recipe, Diet} = require('../db.js') //importamos los modelos
-
-
+//const { v4: uuidv4 } = require('uuid');
 
 const getAllRecipes = async () => {
   try{
@@ -33,47 +36,84 @@ const getAllRecipes = async () => {
 };
 
 
-const getInfoId= async (idRecipe) => {
-try {
-  const findIdDB = await Recipe.findByPk(idRecipe, {
-    include: {
-      model: Diet,
-      attributes: ["name"],
-      through: { diets: [] },
-    },
-  });
+// Definimos la función encargada de traer la información de la APImy guardarla 
+/*const getAllRecipes= async ()=>{
+    const response = await axios.get(`https://api.spoonacular.com/recipes/complexSearch?apiKey=${API_KEY}&addRecipeInformation=true&number=100`);
+    const apiInfo = response.data.results.map((receta) => {
+        const {  title, image, summary, healthScore, analyzedInstructions, diets } = receta;
+        const id = uuidv4();
+        return {
+            id,
+            title,
+            image,
+            summary,
+            healthScore,
+            steps: analyzedInstructions,
+            diets
+        };
+    });
 
-  // Extrae los nombres de las dietas en forma de matriz
+    // Guardar las recetas en la base de datos
+    //await Recipe.bulkCreate(apiInfo);
 
-  const diets = findIdDB.diets.map((diet) => diet.name);
-
-  return { ...findIdDB.toJSON(), diets };
-} catch (error) {
-  console.log("Error al obtener la receta de la base de datos:", error);
-}
+    return apiInfo;
 };
+*/
+const getInfoId= async (idRecipe) => {
+  try {
+    const findIdDB = await Recipe.findByPk(idRecipe, {
+      include: {
+        model: Diet,
+        attributes: ["name"],
+        through: { diets: [] },
+      },
+    });
+  
+    // Extrae los nombres de las dietas en forma de matriz
+  
+    const diets = findIdDB.diets.map((diet) => diet.name);
+  
+    return { ...findIdDB.toJSON(), diets };
+  } catch (error) {
+    console.log("Error al obtener la receta de la base de datos:", error);
+  }
+  };
+  
+  //este funcion va traer todas las dietas
+  const getAllDiets=async()=>{
+      const dietsSet= [...new Set([])]
+      
+      dietsSet.forEach(diet=>{
+          Diet.find({
+              where:{
+                  name: diet
+              }
+          })
+      })
+      const allDiets= await Diet.findAll();
+      return allDiets
+  }
+  
 
-//este funcion va traer todas las dietas
-const getAllDiets=async()=>{
-    const dietsSet= [...new Set([])]
-    
-    dietsSet.forEach(diet=>{
-        Diet.find({
-            where:{
-                name: diet
+//trae las recetas y las guarada en la db
+/*const getAllDiets=async()=>{
+  const info= await getAllRecipes();
+  const diets= info.map(recipe=> recipe.diets)
+  const dietsSet= [...new Set([].concat(...diets))]
+  dietsSet.forEach(diet=>{
+      Diet.findOrCreate({
+          where:{
+              name: diet
             }
-        })
-    })
-    const allDiets= await Diet.findAll();
-    return allDiets
-}
-
-
-
-
+          })
+      })
+      const allDiets= await Diet.findAll();
+      return allDiets
+}*/
 
 module.exports={ // Exportamos las funciones getAllRecipes, getApiInfo y getDBInfo
     getAllRecipes,
-    getInfoId,
-    getAllDiets
+    getAllDiets,
+    getInfoId
 }
+
--- PI-Food-main/api/src/db.js
+++ PI-Food-main/api/src/db.js
@@ -1,26 +1,27 @@
-//La responsabilida de este archivo es levamtar la base de datos, asociarla con el servidor y
-//crear las tablas y las relaciones
-
 require('dotenv').config(); // Cargar las variables de entorno del archivo .env
 const { Sequelize } = require('sequelize'); // Importar el objeto Sequelize
 const fs = require('fs'); // Módulo de Node.js para trabajar con el sistema de archivos
 const path = require('path'); // Módulo de Node.js para trabajar con rutas de archivos
-const { DB_USER, DB_PASSWORD, DB_HOST, DB_NAME } = process.env; // Obtener las variables de entorno
+const { DB_URL } = process.env; // Obtener las variables de entorno
 
 
-console.log(DB_HOST)
-console.log(DB_USER)
-console.log(DB_PASSWORD)
-console.log(DB_NAME)
-// Crear una nueva instancia de Sequelize para conectarnos a la base de datos
+
+// Crear una nueva instancia de Sequelize para conectarnos a la base de datos en Render
+
 const sequelize = new Sequelize(
-  `postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/${DB_NAME}`, 
+  `${ DB_URL }`,
   {
-  
-  logging: false, // Desactivar el logging de SQL queries
-  native: false // Usar pg-native para mejorar la performance
+    logging: false,
+    native: false,
+    dialectOptions: {
+      ssl: {
+        require: true, // Requerir SSL/TLS
+        rejectUnauthorized: false, // Deshabilitar la verificación del certificado (para desarrollo)
+      },
+    },
   }
 );
+
 
 const basename = path.basename(__filename); // Obtener el nombre del archivo actual
 
@@ -33,7 +34,7 @@ fs.readdirSync(path.join(__dirname, '/models'))
     modelDefiners.push(require(path.join(__dirname, '/models', file)));
   });
 
-// Injectar la conexión (sequelize) a todos los modelos
+// Inyectar la conexión (sequelize) a todos los modelos
 modelDefiners.forEach((model) => model(sequelize));
 
 // Capitalizar los nombres de los modelos, por ejemplo 'recipe' => 'Recipe'
@@ -43,23 +44,14 @@ sequelize.models = Object.fromEntries(capsEntries);
 
 // En sequelize.models están todos los modelos importados como propiedades
 // Para relacionarlos hacemos un destructuring
-
-
-
 const { Recipe, Diet } = sequelize.models;
 
+// Aquí se pueden definir las relaciones entre los modelos, como Recipe.belongsToMany(Ingredient)
 
-
-
-
-
-// Aquí se podrcb cían definir las relaciones entre los modelos, como Recipe.hasMany(Ingredient)
-
-Recipe.belongsToMany(Diet, {through: 'RecipeDiet'});
-Diet.belongsToMany(Recipe, {through: 'RecipeDiet'});
-
+Recipe.belongsToMany(Diet, { through: 'RecipeDiet' });
+Diet.belongsToMany(Recipe, { through: 'RecipeDiet' });
 
 module.exports = {
   ...sequelize.models, // Para poder importar los modelos así: const { Recipe, Ingredient } = require('./db.js');
   conn: sequelize, // Para importar la conexión así: { conn } = require('./db.js');
-}; 
+};
